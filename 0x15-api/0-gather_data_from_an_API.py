03f007922e5ea154e86e1bc06b532b963e6497eb
Webnai <wilbertboadzo144@gmail.com>
2023-07-24 10:49:20 -0700

    gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,43 +1,14 @@
 #!/usr/bin/python3
-"""to do list information about an employee """
-
+"""Returns to-do list information for a given employee ID."""
 import requests
 import sys
 
-base = 'https://jsonplaceholder.typicode.com/'
-
-
-def tasks_employee():
-    """get tasks done of an employee"""
-    if len(sys.argv) < 2:
-        print("Missing id")
-        return
-    try:
-        id_e = int(sys.argv[1])
-    except ValueError:
-        print("id must be an integer")
-        return
-    req = requests.get(base + 'users/' + str(id_e))
-    user = req.json()
-    req2 = requests.get(base + 'todos/')
-    todos = req2.json()
-    tasks = []
-    finished = 0
-    not_finished = 0
-    total = 0
-
-    for todo in todos:
-        if todo.get('userId') == id_e:
-            if todo.get('completed') is True:
-                finished = finished + 1
-                tasks.append(todo.get('title'))
-            else:
-                not_finished = not_finished + 1
-    total = finished + not_finished
-    print("Employee {} is done with tasks({}/{}):".format(user.get('name'),
-                                                          finished, total))
-    for task in tasks:
-        print("\t {}".format(task))
-
 if __name__ == "__main__":
-    tasks_employee()
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
