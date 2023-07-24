b0cf1beaac21780991a4573f782840ec65993253
Webnai <wilbertboadzo144@gmail.com>
2023-07-24 10:53:42 -0700

    export to JSON"

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,42 +1,19 @@
 #!/usr/bin/python3
-"""to do list information about an employee export to json """
-
+"""Exports to-do list information for a given employee ID to JSON format."""
 import json
 import requests
 import sys
 
-base = 'https://jsonplaceholder.typicode.com/'
-
-
-def tasks_employee():
-    """get tasks of an employee"""
-    if len(sys.argv) < 2:
-        print("Missing id")
-        return
-    try:
-        id_e = int(sys.argv[1])
-    except ValueError:
-        print("id must be an integer")
-        return
-    req = requests.get(base + 'users/' + str(id_e))
-    user = req.json()
-    req2 = requests.get(base + 'todos/')
-    todos = req2.json()
-    mylist = []
-    mydict = {}
-    lista = []
-    mydict_final = {}
-    for todo in todos:
-        if todo.get('userId') == id_e:
-            lista.append(todo)
-    user_name = user.get('username')
-    mylist = [{'task': todo.get('title'),
-               'completed': todo.get('completed'),
-               'username': user.get('username')}
-              for todo in lista]
-    mydict_final[str(id_e)] = mylist
-    with open(str(id_e) + '.json', 'w') as f:
-        json.dump(mydict_final, f)
-
 if __name__ == "__main__":
-    tasks_employee()
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump({user_id: [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": username
+            } for t in todos]}, jsonfile)
