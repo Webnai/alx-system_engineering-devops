bbf492c335ab659728befc695aff64faf0122358
Webnai <wilbertboadzo144@gmail.com>
2023-08-09 13:10:48 -0700

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-"""A function that queries the Reddit API and
-returns the number of subscribers"""
+""" total of subscribers"""
+
 import requests
 
 
+base_url = 'http://reddit.com/r/{}/about.json'
+
+
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    """number of suscribers"""
+    headers = {'User-agent': 'karenahv'}
+    req = requests.get(base_url.format(subreddit), headers=headers)
+    if req.status_code != 200:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers`")
+    return req.json().get('data').get('subscribers')
